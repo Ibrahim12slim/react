02a1048d1ae7ff0f6ffdd984b807231751433e31
Ibrahim12slim <96397533+Ibrahim12slim@users.noreply.github.com>
2024-01-22 09:45:06 +0200

    UseState() method usage

---
+++ 2react/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+
+function App (){
+
+    const [books , setbook] = useState(10)
+
+    const sellBook = () => {
+        if (books === 0) {
+            setbook(books)
+        }
+        else{
+            setbook(books - 1)
+        }
+        
+    }
+
+    const returnBook = () => {
+        if (books === 10) {
+            setbook(books)
+        }
+        else{
+            setbook(books + 1)
+        }
+    }
+
+    return (
+        <>
+            <h2>Books in the library : {books} </h2>
+            <button onClick = {sellBook}
+            >Sell Book </button>{" "}
+            <button onClick = {returnBook}
+            >Return Book </button>
+        </>
+    )
+}
+
+export default App
--- 2react/src/index.js
+++ 2react/src/index.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const areactElemenet = React.createElement(
-    'a',
-    {href : 'http://google.com' , target : '_blank'},
-    'click to visit'
-)
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    areactElemenet
+    <App/>
 );
